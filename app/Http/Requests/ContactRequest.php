5919279b02c25f144bec24d97750be1c0531cfc7
grenka <supermazila1204@gmail.com>
2026-07-16 22:06:12 +1100

    Add request validation for contact form

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -7,23 +7,39 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string','min:2' ,'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'min:6', 'max:20'],
+            'comment' => ['required', 'string', 'min:10', 'max:1000'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.string' => 'The name must be a string.',
+            'name.min' => 'The name must be at least :min characters.',
+            'name.max' => 'The name may not be greater than :max characters.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.max' => 'The email may not be greater than :max characters.',
+            'phone.required' => 'The phone field is required.',
+            'phone.string' => 'The phone must be a string.',
+            'phone.min' => 'The phone must be at least :min characters.',
+            'phone.max' => 'The phone may not be greater than :max characters.',
+            'comment.required' => 'The comment field is required.',
+            'comment.string' => 'The comment must be a string.',
+            'comment.min' => 'The comment must be at least :min characters.',
+            'comment.max' => 'The comment may not be greater than :max characters.',
         ];
     }
 }
